8562cb36960e6470064cbb458cb860312c4ffb35
Ajay Saxena <142115308+Ajaysaxena976@users.noreply.github.com>
2024-04-30 17:57:58 +0530

    Time: 5 ms (21.31%), Space: 7.5 MB (16.15%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int reverse(int n) {
-         if(    (n > pow(2,31)  - 1  )  || ( n<-( pow(2,31) )   )  )
-    {
-        return -1;
-    }
-    else
-    {
+     
+    
         int digit,ans=0;
         while(n!=0)
         {
+            if ( (ans> INT_MAX/10) | (ans < INT_MIN/10) ){
+                return 0;
+            }
             digit=n%10;
             ans=10*ans+digit;
             n=n/10;
         }
         return ans;
     }
-    }
+    
 };
